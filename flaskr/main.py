b064d6149c03fcd3b4fa916681efaad400b309e9
imai <115885856+M-Imai01@users.noreply.github.com>
2022-10-21 23:31:50 +0900

    SQLite Deployment

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,8 +1,14 @@
 from flaskr import app
 from flask import render_template
+import sqlite3
+DATABASE = 'database.db'
 
 @app.route('/')
 def index():
+    con = sqlite3.connect(DATABASE)
+    db_books = con.execute('SELECT * FROM books').fetchall()
+    con.close()
+    
     books = [
         # {'title': 'はらぺこあおむし',
         #  'price': 1200,
@@ -11,6 +17,9 @@ def index():
         #  'price': 990,
         #  'arrival_day': '2020年7月13日'},
     ]
+    for row in db_books:
+        books.append({'title': row[0], 'price': row[1], 'arrival_day': row[2]})
+    
     return render_template(
         'index.html',
         books=books
