ba768eb6b0c83632ceccdc97a0397fa4dfb548c4
imai <115885856+M-Imai01@users.noreply.github.com>
2022-10-21 18:40:24 +0900

    for statement in the template engine

--- flaskr/main.py
+++ flaskr/main.py
@@ -3,12 +3,15 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    book = {
-        'title': 'はらぺこあおむし',
-        'price': 1200,
-        'arrival_day': '2020年7月12日'
-    }
+    books = [
+        {'title': 'はらぺこあおむし',
+         'price': 1200,
+         'arrival_day': '2020年7月12日'},
+        {'title': 'ぐりとぐら',
+         'price': 990,
+         'arrival_day': '2020年7月13日'},
+    ]
     return render_template(
         'index.html',
-        book=book
+        books=books
     )
