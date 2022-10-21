39a3d0ea03ac36b8e978b088a09e6e9b05d4eaf8
imai <115885856+M-Imai01@users.noreply.github.com>
2022-10-21 20:18:28 +0900

    Transition to new page

--- flaskr/main.py
+++ flaskr/main.py
@@ -4,14 +4,20 @@ from flask import render_template
 @app.route('/')
 def index():
     books = [
-        {'title': 'はらぺこあおむし',
-         'price': 1200,
-         'arrival_day': '2020年7月12日'},
-        {'title': 'ぐりとぐら',
-         'price': 990,
-         'arrival_day': '2020年7月13日'},
+        # {'title': 'はらぺこあおむし',
+        #  'price': 1200,
+        #  'arrival_day': '2020年7月12日'},
+        # {'title': 'ぐりとぐら',
+        #  'price': 990,
+        #  'arrival_day': '2020年7月13日'},
     ]
     return render_template(
         'index.html',
         books=books
+    )
+    
+@app.route('/form')
+def form():
+        return render_template(
+        'form.html'
     )
