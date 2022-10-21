9f189302b008cc07865682e4103e069fbfe89908
imai <115885856+M-Imai01@users.noreply.github.com>
2022-10-22 00:42:13 +0900

    Equipped with data registration function

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,5 +1,7 @@
+# from crypt import methods
+# from turtle import title
 from flaskr import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 import sqlite3
 DATABASE = 'database.db'
 
@@ -9,14 +11,7 @@ def index():
     db_books = con.execute('SELECT * FROM books').fetchall()
     con.close()
     
-    books = [
-        # {'title': 'はらぺこあおむし',
-        #  'price': 1200,
-        #  'arrival_day': '2020年7月12日'},
-        # {'title': 'ぐりとぐら',
-        #  'price': 990,
-        #  'arrival_day': '2020年7月13日'},
-    ]
+    books = []
     for row in db_books:
         books.append({'title': row[0], 'price': row[1], 'arrival_day': row[2]})
     
@@ -30,3 +25,17 @@ def form():
         return render_template(
         'form.html'
     )
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    title = request.form['title']
+    price = request.form['price']
+    arrival_day = request.form['arrival_day']
+    
+    con = sqlite3.connect(DATABASE)
+    con.execute('INSERT INTO books VALUES(?, ?, ?)',
+                [title, price, arrival_day])
+    con.commit()
+    con.close()
+    return redirect(url_for('index'))
